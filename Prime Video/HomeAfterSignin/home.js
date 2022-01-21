d05ad3c538c9c528e30c64cd8762f35305315d7c
Rampukar Yadav <imaryan08@gmail.com>
2022-01-22 00:45:19 +0530

    prime video folder added
    22Jan2022 , home page, signin page, homepage after signin

---
+++ Prime Video/HomeAfterSignin/home.js
@@ -0,0 +1,33 @@
+function signIn() {
+    window.location.href = "/SIGN-IN/signin.html";
+  }
+  
+  var favChannels = JSON.parse(localStorage.getItem("favChannels"));
+  
+  favChannels.map(function (elem, index) {
+    var div = document.createElement("div");
+    div.setAttribute("class", "channelDiv");
+    var image = document.createElement("img");
+    image.setAttribute("src", elem.img_url);
+  
+    div.append(image);
+    document.querySelector(".channelBtn").append(div);
+  
+    div.addEventListener("click", function () {
+      window.location.href = `/channels/channel${index + 1}.html`;
+    });
+  });
+  
+  // function createPage(){
+  //     // Section
+  //     var section = document.createElement('section');
+  //     // Image
+  //     var image = document.createElement('img');
+  //     image.setAttribute('src','');
+  
+  //     // textOverImage
+  //     var textOverImage = document.createElement('div');
+  //     textOverImage.setAttribute('id','membershipDiv');
+  
+  // }
+  
